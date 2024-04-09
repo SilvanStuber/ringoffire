faf4983cf5469ef986bdde900862287bba65bcae
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-04-09 21:35:42 +0200

    show which player is active

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { GameComponent } from '../game/game.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-player',
   standalone: true,
-  imports: [GameComponent],
+  imports: [GameComponent, NgClass],
   templateUrl: './player.component.html',
   styleUrl: './player.component.scss'
 })
 export class PlayerComponent {
 
   @Input() name: any;
+  @Input() playerActive: boolean = false;
 
 }
