69b26da37dd2be3854e9718d708480efd4274f0a
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-04-09 20:36:45 +0200

    add button und dialog window

---
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import {
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+@Component({
+  selector: 'app-dialog-add-player',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
+  templateUrl: './dialog-add-player.component.html',
+  styleUrl: './dialog-add-player.component.scss',
+})
+export class DialogAddPlayerComponent {
+  name: string = '';
+
+  onNoClick(): void {
+    
+  }
+}
